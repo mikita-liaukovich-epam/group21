c1737536cf6c1460f19722d69b4c5bddced18072
Nick Levkovich <nicklevkovic@gmail.com>
2019-01-24 22:28:47 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# CodeJam-Culture-Portal
+## Deadline: Monday, Jan 28, 2019 23:59
+
+## Task
+Realise the portal of culture of Belarus, working in a group and on the topic issued.
+
+The goal is to quickly make content project using available simple tools, distributing work and helping each other.
+
+Preview: https://nicklevkovich.github.io/group21/
+
+Trello:
+[Trello board](https://trello.com/b/XOiPpISU/cj-culture-portal "Trello board")
---
+++ webpack.config.js
@@ -0,0 +1,68 @@
+/* eslint-disable */
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+const PUBLIC_DIR = 'public';
+
+module.exports = {
+  mode: 'development',
+  entry: {
+    main: './src/index.js'
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: './' + PUBLIC_DIR,
+    port: 9000,
+  },
+  plugins: [
+    new CleanWebpackPlugin([PUBLIC_DIR]),
+    new ExtractTextPlugin({
+      filename: './style.css',
+      allChunks: true,
+    }),
+    new HtmlWebpackPlugin({
+      title: 'Development',
+      filename: 'index.html'
+    }),
+    new CopyWebpackPlugin([
+      { from: './assets', to: './assets' }
+    ]),
+  ],
+  output: {
+    path: path.resolve(__dirname, PUBLIC_DIR)
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js/,
+        loader: 'babel-loader'
+      },
+      {
+        test: /\.(png|jpg|gif)$/,
+        use: [{
+          loader: 'file-loader',
+          options: {
+            name: './images/[name].[ext]',
+          },
+        }],
+      },
+      {
+        test: /\.css$/,
+        loader: ExtractTextPlugin.extract({
+          use: [{
+              loader: "css-loader",
+              options: {
+                sourceMap: true,
+                minimize: true,
+                url: false
+              }
+            }
+          ]
+        })
+      }
+  ]
+  }
+};
