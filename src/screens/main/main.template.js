f81a828c28ee572452fd568f586a3f302ab1fcc7
Nick Levkovich <nicklevkovic@gmail.com>
2019-01-26 20:47:08 +0300

    feat: add first screen of landing page with search and language choosing

---
+++ src/screens/main/main.template.js
@@ -0,0 +1,23 @@
+export default `
+  <section class="first-page">
+    <div class="container">
+      <img src="./assets/ENG.png" alt="Language" class="active-lang">
+      <div class="languages">
+        <img src="./assets/ENG.png" alt="English language" class="eng">
+        <img src="./assets/BEL.png" alt="Belarussian language" class="bel">
+        <img src="./assets/RUS.png" alt="Russian language" class="rus">
+      </div>
+      <nav>
+          <a href="#">List of poets</a>
+          <a href="#about">About Us</a>
+          <a href="#author-of-the-day">Author of the Day</a>
+          <a href="#">Contacts</a>
+        </nav>
+      <img src="./assets/logo.png" alt="Poets of Belarus" class="logo">
+      <div class="buttons-wrapper">
+        <button class='main-button left'>List of poets</button>
+        <input type="text" class='main-button search-string' placeholder="Search">
+      </div>
+    </div>
+  </section>
+`
