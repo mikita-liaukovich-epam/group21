6314e10d1e62b37a87349adcac29d5a559a2d614
kir-22 <kirill89kireew@gmail.com>
2019-01-26 22:52:04 +0300

    feat: added the template list

---
+++ src/screens/main/list/main_kir.template.js
@@ -0,0 +1,37 @@
+export default `
+<div class="logo"></div>
+  <main class = 'content'>
+  <section class = 'list'>
+      <h3>Б</h3>
+      <ul class="first">
+        <li><a href="" class="poets-name">Бровка, Пётр Устинович</a></li>
+      </ul>
+    </section>
+    <section class = 'list'>
+      <h3>Д</h3>
+      <ul class="first">
+        <li><a href="" class="poets-name">Дунин-Марцинкевич, Викентий Иванович</a></li>
+      </ul>
+    </section>
+    <section class = 'list'>
+      <h3>К</h3>
+      <ul class="first">
+        <li><a href="" class="poets-name">Короткевич, Владимир Семёнович</a></li>
+      </ul>
+    </section>
+    <section class = 'list'>
+      <h3>М</h3>
+      <ul class="first">
+        <li><a href="" class="poets-name">Максим Танк</a></li>
+      </ul>
+    </section>
+    <section class = 'list'>
+      <h3>Я</h3>
+      <ul class="first">
+        <li><a href="" class="poets-name">Якуб Колас</a></li>
+        <li><a href="" class="poets-name">Янка Купала</a></li>
+        <li><a href="" class="poets-name">Янка Лучина</a></li>
+      </ul>
+    </section>
+  </main>
+`
