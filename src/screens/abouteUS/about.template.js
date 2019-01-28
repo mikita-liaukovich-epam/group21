450a44ddabfd826419faeff9fd354d8864e5e9f4
kir-22 <kirill89kireew@gmail.com>
2019-01-28 19:54:33 +0300

    feat: added change language on site contact

--- src/screens/abouteUS/about.template.js
+++ src/screens/abouteUS/about.template.js
@@ -1,5 +1,5 @@
-export default `
-<div class="lists-container">
+export default { eng: `
+<div class="user-body">
   <img src='./assets/developer/devs.png' class='logotype'>
   <main class = 'user-container'>
     <a class = "close"> &#10006;</a>
@@ -56,4 +56,122 @@ export default `
       </section>
   </main>
 </div>
-`
+`,
+bel: `
+<div class="user-body">
+  <img src='./assets/developer/devs.png' class='logotype'>
+  <main class = 'user-container'>
+    <a class = "close"> &#10006;</a>
+    <section class="user-content">
+        <h3>Мікалай Ляўковіч</h3>
+        <div class="user-card">
+          <div class = "user-foto"></div>
+          <div class = "user-info">
+            <h4>Малодшы Front-end распрацоўшчык</h4>
+            <p class = "user-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt»</p>
+            <p class = "user-social">
+              <a href="" class="link-social"><span></span></a>
+              <a href="" class="link-social"><span></span></a>
+              <a href="" class="link-social"><span></span></a>
+              <a href="" class="link-social"><span></span></a>
+              <a href="" class="link-social"><span></span></a>
+            </p>
+          </div>
+        </div>
+      </section>
+      <section class="user-content">
+        <h3>Кацярына Макаранка</h3>
+        <div class="user-card">
+          <div class = "user-foto"></div>
+          <div class = "user-info">
+            <h4>Малодшы Front-end распрацоўшчык</h4>
+            <p class = "user-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt»</p>
+            <p class = "user-social">
+              <a href="" class="link-social"><span></span></a>
+              <a href="" class="link-social"><span></span></a>
+              <a href="" class="link-social"><span></span></a>
+              <a href="" class="link-social"><span></span></a>
+              <a href="" class="link-social"><span></span></a>
+            </p>
+          </div>
+        </div>
+      </section>
+      <section class="user-content">
+        <h3>Кірыл Кірэеў</h3>
+        <div class="user-card">
+          <div class = "user-foto"></div>
+          <div class = "user-info">
+            <h4>Малодшы Front-end распрацоўшчык</h4>
+            <p class = "user-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt»</p>
+            <p class = "user-social">
+              <a href="" class="link-social"><span></span></a>
+              <a href="" class="link-social"><span></span></a>
+              <a href="" class="link-social"><span></span></a>
+              <a href="" class="link-social"><span></span></a>
+              <a href="" class="link-social"><span></span></a>
+            </p>
+          </div>
+        </div>
+      </section>
+  </main>
+</div>
+`,
+rus: `
+<div class="user-body">
+  <img src='./assets/developer/devs.png' class='logotype'>
+  <main class = 'user-container'>
+    <a class = "close"> &#10006;</a>
+    <section class="user-content">
+        <h3>Николай Левкович</h3>
+        <div class="user-card">
+          <div class = "user-foto"></div>
+          <div class = "user-info">
+            <h4>Младший Фронт-энд разработчик</h4>
+            <p class = "user-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt»</p>
+            <p class = "user-social">
+              <a href="" class="link-social"><span></span></a>
+              <a href="" class="link-social"><span></span></a>
+              <a href="" class="link-social"><span></span></a>
+              <a href="" class="link-social"><span></span></a>
+              <a href="" class="link-social"><span></span></a>
+            </p>
+          </div>
+        </div>
+      </section>
+      <section class="user-content">
+        <h3>Екатерина Макаренко</h3>
+        <div class="user-card">
+          <div class = "user-foto"></div>
+          <div class = "user-info">
+            <h4>Младший Фронт-энд разработчик</h4>
+            <p class = "user-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt»</p>
+            <p class = "user-social">
+              <a href="" class="link-social"><span></span></a>
+              <a href="" class="link-social"><span></span></a>
+              <a href="" class="link-social"><span></span></a>
+              <a href="" class="link-social"><span></span></a>
+              <a href="" class="link-social"><span></span></a>
+            </p>
+          </div>
+        </div>
+      </section>
+      <section class="user-content">
+        <h3>Кирилл Киреев</h3>
+        <div class="user-card">
+          <div class = "user-foto"></div>
+          <div class = "user-info">
+            <h4>Младший Фронт-энд разработчик</h4>
+            <p class = "user-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt»</p>
+            <p class = "user-social">
+              <a href="" class="link-social"><span></span></a>
+              <a href="" class="link-social"><span></span></a>
+              <a href="" class="link-social"><span></span></a>
+              <a href="" class="link-social"><span></span></a>
+              <a href="" class="link-social"><span></span></a>
+            </p>
+          </div>
+        </div>
+      </section>
+  </main>
+</div>
+` }
