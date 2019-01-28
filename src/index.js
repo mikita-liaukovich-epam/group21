4f1e9eccf31d51c6fe6769eefc2966070fff84ff
Kate Makarenka <polumna-la@mail.ru>
2019-01-28 05:20:05 +0300

    fix: fixing bags

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import './screens/main/main';
+// import './screens/main/list/main_kir';
 import './screens/list/main_kir';
 import './screens/abouteUS/abouteUs';
 import './screens/abouteUS/abouteUs';
