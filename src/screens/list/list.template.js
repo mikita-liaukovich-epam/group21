59b560a3c2ab61a52a483cae026a7c52f76cd364
kir-22 <kirill89kireew@gmail.com>
2019-01-28 19:17:02 +0300

    feat: added the change language in lists poets

--- src/screens/list/list.template.js
+++ src/screens/list/list.template.js
@@ -1,4 +1,84 @@
-export default `
+export default { eng: `
+<div class="lists-container">
+  <img src='./assets/logo/logo.png' class="logotype"></div>
+  <main class = 'content'>
+  <a class = "close"> &#10006;</a>
+  <section class = 'list'>
+      <h3>B</h3>
+      <ul class="first">
+        <li><a href="#" class="poets-name" data-name="brovka" >Petrus Brovka</a></li>
+      </ul>
+    </section>
+    <section class = 'list'>
+      <h3>D</h3>
+      <ul class="first">
+        <li><a href="#" class="poets-name" data-name="dunin">Vintsent Dunin-Martsinkyevich</a></li>
+      </ul>
+    </section>
+    <section class = 'list'>
+      <h3>К</h3>
+      <ul class="first">
+        <li><a href="#" class="poets-name" data-name="korotkevich">Vladimir Korotkevich</a></li>
+        <li><a href="#" class="poets-name" data-name="kolas">Yakub Kolas</a></li>
+        <li><a href="#" class="poets-name" data-name="kupala">Yanka Kupala</a></li>
+      </ul>
+    </section>
+    <section class = 'list'>
+    <h3>Л</h3>
+    <ul class="first">
+      <li><a href="#" class="poets-name" data-name="luchina">Yanka Luchina</a></li>
+    </ul>
+    </section>
+    <section class = 'list'>
+      <h3>Т</h3>
+      <ul class="first">
+        <li><a href="#" class="poets-name" data-name="tank">Maksim Tank</a></li>
+      </ul>
+    </section>
+  </main>
+</div>
+`,
+bel: `
+<div class="lists-container">
+  <img src='./assets/logo/logo.png' class="logotype"></div>
+  <main class = 'content'>
+  <a class = "close"> &#10006;</a>
+  <section class = 'list'>
+      <h3>Б</h3>
+      <ul class="first">
+        <li><a href="#" class="poets-name" data-name="brovka" >Пятрусь Броўка</a></li>
+      </ul>
+    </section>
+    <section class = 'list'>
+      <h3>Д</h3>
+      <ul class="first">
+        <li><a href="#" class="poets-name" data-name="dunin">Вінцэнт Дунін-Марцінкевіч</a></li>
+      </ul>
+    </section>
+    <section class = 'list'>
+      <h3>К</h3>
+      <ul class="first">
+        <li><a href="#" class="poets-name" data-name="korotkevich">Уладзімір Сямёнавіч Караткевіч</a></li>
+        <li><a href="#" class="poets-name" data-name="kolas">Якуб Колас</a></li>
+        <li><a href="#" class="poets-name" data-name="kupala">Янка Купала</a></li>
+      </ul>
+    </section>
+    <section class = 'list'>
+    <h3>Л</h3>
+    <ul class="first">
+      <li><a href="#" class="poets-name" data-name="luchina">Янка Лучына</a></li>
+    </ul>
+    </section>
+    <section class = 'list'>
+      <h3>Т</h3>
+      <ul class="first">
+        <li><a href="#" class="poets-name" data-name="tank">Максім Танк</a></li>
+      </ul>
+    </section>
+  </main>
+</div>
+`,
+rus: `
 <div class="lists-container">
   <img src='./assets/logo/logo.png' class="logotype"></div>
   <main class = 'content'>
@@ -37,4 +117,4 @@ export default `
     </section>
   </main>
 </div>
-`
+` }
